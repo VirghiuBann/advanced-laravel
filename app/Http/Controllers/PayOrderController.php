c92ae8bc869cbb589ede8b3899eab45c2d6912e1
virghiu <virghiu@gmail.com>
2021-05-14 22:14:53 -0400

    refactor and implement interface

--- app/Http/Controllers/PayOrderController.php
+++ app/Http/Controllers/PayOrderController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Billing\PaymentGateway;
+use App\Billing\PaymentGatewayContract;
+use App\Orders\OrderDetails;
 use Illuminate\Http\Request;
 
 class PayOrderController extends Controller
 {
-    public function store(PaymentGateway $paymentGateway)
+    public function store(OrderDetails $orderDetails, PaymentGatewayContract $paymentGateway)
     {
         // PaymentGateway $paymentGateway
         // $paymentGateway = new PaymentGateway('usd');
+        $order = $orderDetails->all();
 
         dd($paymentGateway->charge(2500));
     }
