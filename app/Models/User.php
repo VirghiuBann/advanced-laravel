f13e7c2cce5342d996e97c120f255f4d18a1dc06
virghiu <virghiu@gmail.com>
2021-05-17 11:55:10 -0400

    polymorphic relations shios

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
